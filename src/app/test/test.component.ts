f1c25b80404d61c87ba75e711afe47e14bc64d0c
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2018-08-12 17:46:45 +0530

    Class Binding and Property Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-
+  public name = 'Tanmay';
+  public isError = false;
+  public isSpecial = true;
+  public message = {
+  	'text-special' : this.isSpecial,
+  	'text-error' : this.isError,
+  	'text-success' : !this.isError
+  };
+  public dangerClass = 'text-danger';
+  public isDisabled = 'true';
   constructor() { }
 
   ngOnInit() {
