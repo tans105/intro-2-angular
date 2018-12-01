113d2d42d48cebf8f845e401514cb3ee276d8d40
Tanmay <tanmayawasthu105@gmail.com>
2018-12-01 15:51:15 +0530

    two way data binding

--- sample-app-2/src/app/app.module.ts
+++ sample-app-2/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from "./server/server.component";
@@ -9,7 +10,7 @@ import {ServerComponent} from "./server/server.component";
     AppComponent, ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- sample-app-2/src/app/server/server.component.ts
+++ sample-app-2/src/app/server/server.component.ts
@@ -1,9 +1,11 @@
 import {Component} from "@angular/core";
+import {FormsModule} from "@angular/forms";
 
 @Component({
     selector: "app-server",
     templateUrl: "./server.component.html"
 })
 export class ServerComponent {
-
+    serverId: number = 1;
+    serverName: string = "MyServer";
 }
